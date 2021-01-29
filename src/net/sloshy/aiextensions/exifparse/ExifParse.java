c7a34930e639a46d455b35e7e4248536a0198770
sloshyyy <nate@jankowskis.net>
2021-01-29 18:31:07 -0500

    switched to android standard library
    some parsing works now!

--- src/net/sloshy/aiextensions/exifparse/ExifParse.java
+++ src/net/sloshy/aiextensions/exifparse/ExifParse.java
@@ -1,27 +1,18 @@
 package net.sloshy.aiextensions.exifparse;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
-import com.drew.metadata.Directory;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.lang.Iterable;
-
+import android.media.ExifInterface;
 import com.google.appinventor.components.annotations.DesignerComponent;
-import com.google.appinventor.components.annotations.SimpleObject;
-import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleEvent;
-import com.google.appinventor.components.annotations.SimpleProperty;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
-import com.google.appinventor.components.runtime.AndroidViewComponent;
 import com.google.appinventor.components.runtime.ComponentContainer;
 import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.util.YailDictionary;
+
+import java.io.File;
+import java.io.IOException;
 
 @DesignerComponent(version = 1, 
                     category = ComponentCategory.EXTENSION, 
@@ -41,15 +32,18 @@ public class ExifParse extends AndroidNonvisibleComponent{
     }
 
     @SimpleFunction(description = "Returns exif metadata as a dict")
-    public YailDictionary getExifData(String filePath) throws IOException, ImageProcessingException {
-        Metadata md = ImageMetadataReader.readMetadata(new FileInputStream(filePath));
+    public YailDictionary getExifData(String filePath) throws IOException {
+        ExifInterface ei = new ExifInterface(new File(filePath));
         YailDictionary yd = new YailDictionary();
-        for (Directory directory : md.getDirectories()) {
-            for (Tag tag : directory.getTags()) {
-                yd.put(directory, tag);
-            }
-        }
+
+
+
 
         return yd;
+    }
+    @SimpleFunction
+    public String getDate(String filePath) throws IOException {
+        ExifInterface ei = new ExifInterface(new File(filePath));
+        return ei.getAttribute(ExifInterface.TAG_DATETIME);
     }
 }
