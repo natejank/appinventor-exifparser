5b01d2e0cba40fd06c28d7ceb9a82f441a0822d0
sloshyyy <nate@jankowskis.net>
2021-01-31 10:49:47 -0500

    cleaned up and added write functionality
    I've removed a lot of methods as to make things as simple as possible,
    so most of the logic will stay within appinventor

--- src/net/sloshy/aiextensions/exifparse/ExifParse.java
+++ src/net/sloshy/aiextensions/exifparse/ExifParse.java
@@ -1,112 +1,90 @@
 package net.sloshy.aiextensions.exifparse;
 
 import android.media.ExifInterface;
-import com.google.appinventor.components.annotations.DesignerComponent;
-import com.google.appinventor.components.annotations.SimpleEvent;
-import com.google.appinventor.components.annotations.SimpleFunction;
-import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
 import com.google.appinventor.components.runtime.ComponentContainer;
 import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.util.YailDictionary;
 import com.google.appinventor.components.runtime.util.YailList;
-import com.google.appinventor.components.annotations.SimpleProperty;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collection;
 
 
+/*This extension is intended to be nothing more than a wrapper around android.media.ExifInterface, as to allow you to
+* read and write exif metadata in appinventor.  This is why it might be lacking some nice to haves, as I wanted to keep
+* as much logic as possible within appinventor, leaving only the things that wouldn't be possible to the
+* extension.*/
 @DesignerComponent(version = 1,
         category = ComponentCategory.EXTENSION,
         description = "An extension to allow for parsing of EXIF metadata within appinventor",
-        nonVisible = true, iconName="")
+        nonVisible = true, iconName = "")
 @SimpleObject(external = true)
 public class ExifParse extends AndroidNonvisibleComponent {
 
-    public ExifInterface image = null;
-    public YailDictionary imageAttributes = null;
+    public static final String[] EXIF_ATTRIBUTES = {
+            ExifInterface.TAG_DATETIME,
+            ExifInterface.TAG_GPS_LATITUDE,
+            ExifInterface.TAG_GPS_LONGITUDE,
+            ExifInterface.TAG_EXPOSURE_TIME,
+            ExifInterface.TAG_GPS_ALTITUDE,
+            ExifInterface.TAG_GPS_ALTITUDE_REF,
+            ExifInterface.TAG_GPS_LATITUDE,
+            ExifInterface.TAG_GPS_LONGITUDE,
+            ExifInterface.TAG_DEVICE_SETTING_DESCRIPTION,
+            ExifInterface.TAG_IMAGE_UNIQUE_ID
+    };
 
     public ExifParse(ComponentContainer container) {
         super(container.$form());
     }
 
-    @SimpleEvent(description = "Handle errors that occur from the library")
-    public void ErrorOccured(String error) {
-        EventDispatcher.dispatchEvent(this, "ErrorOccured", error);
+    /* I don't know if this is the *best* way to do this, I just needed some way to ensure that you can't be trying to
+     * use a null file.  The best way I could find to do that was to always make the user input a filepath, rather than
+     * trying to handle it in the class itself. */
+    private static ExifInterface getExifInterface(String filePath) throws IOException {
+        return new ExifInterface(new File(filePath));
     }
 
-    @SimpleFunction()
-    public void setImage(String filePath) throws IOException {
-        if (new File(filePath).exists()) {
-            image = new ExifInterface(new File(filePath));
-            imageAttributes = getAttributes(image);
-        } else {
-            throw new FileNotFoundException("File could not be opened!");
+    @SimpleFunction(description = "gets the specified attribute.  " +
+            "If the image doesn't have the attribute, returns an empty string.")
+    public static String getAttribute(String tagName, String filePath) throws IOException {
+        ExifInterface image = getExifInterface(filePath);
+        String attr = image.getAttribute(tagName);
+        if (attr == null)
+            return "";
+        return attr;
+    }
+
+    /*While this is more logic that I'd like, I don't see a better way of doing it, because you can't manipulate
+    * ExifInterfaces directly in appinventor, and I've decided to pass in pathnames to all of these functions instead
+    * of having a central ExifInterface variable, as to prevent the headache of forgetting to set one.*/
+    @SimpleFunction(description = "Writes attributes.  Use this sparingly, because it involves " +
+            "copying all the data from one file to another and deleting the old file and renaming the other.")
+    public static void setAttributes(YailDictionary values, String filePath) throws IOException {
+        ExifInterface image = getExifInterface(filePath);
+        // I'm really not sure this is the best way of accomplishing this, but I'm probably going to use it unless it
+        // really doesn't work
+        var keys = values.keySet().toArray();
+          for (Object key : keys) {
+            image.setAttribute((String) key, (String) values.get(key));
         }
+        image.saveAttributes();
     }
 
-    @SimpleFunction()
-    public String getAttribute(String tagName) {
-        return image.getAttribute(tagName);
-    }
-
-    @SimpleProperty
-    public YailDictionary getImageAttributes() {
-        return imageAttributes;
-    }
-
-    private YailDictionary getAttributes(ExifInterface image) {
-//        HashMap<String, Boolean> attrs = new HashMap<String, Boolean>();
-        YailDictionary attrs = new YailDictionary();
-        //eyugh
-        String[] fields = {
-                ExifInterface.TAG_DATETIME,
-                ExifInterface.TAG_GPS_LATITUDE,
-                ExifInterface.TAG_GPS_LONGITUDE,
-                ExifInterface.TAG_EXPOSURE_TIME,
-                ExifInterface.TAG_GPS_ALTITUDE,
-                ExifInterface.TAG_GPS_ALTITUDE_REF,
-                ExifInterface.TAG_GPS_LATITUDE,
-                ExifInterface.TAG_GPS_LONGITUDE,
-                ExifInterface.TAG_DEVICE_SETTING_DESCRIPTION,
-                ExifInterface.TAG_IMAGE_UNIQUE_ID
-        };
-        for (String attr : fields)
-            if (image.getAttribute(attr) != null)
-                attrs.put(attr, image.getAttribute(attr));
-//         // getting gps separately, so it becomes more usable
-        float[] latLong = {0.0f, 0.0f};
-        if (image.getLatLong(latLong))
-            attrs.put("LatLong", latLong);
-
-        return attrs;
-    }
-    @SimpleProperty
-    public YailList EXIF_ATTRIBUTES() {
-        // WHY WOULD YOU USE THESE DUMB ESOTERIC DATATYPES WHEN JAVA ALREADY HAS PERFECTLY ACCEPTABLE ONES BUILT IN ASD;ASDFJKL;ADFSJLK;ADFSJKL;ERSGW
-        var attributes = new YailList();
-        String[] fields = {
-                ExifInterface.TAG_DATETIME,
-                ExifInterface.TAG_GPS_LATITUDE,
-                ExifInterface.TAG_GPS_LONGITUDE,
-                ExifInterface.TAG_EXPOSURE_TIME,
-                ExifInterface.TAG_GPS_ALTITUDE,
-                ExifInterface.TAG_GPS_ALTITUDE_REF,
-                ExifInterface.TAG_GPS_LATITUDE,
-                ExifInterface.TAG_GPS_LONGITUDE,
-                ExifInterface.TAG_DEVICE_SETTING_DESCRIPTION,
-                ExifInterface.TAG_IMAGE_UNIQUE_ID
-        };
-        for (String field : fields)
+    @SimpleProperty(description = "Nets the possible tags that can be stored in an image's metadata." +
+            "  Not all of this will be present in every image.")
+    public static YailList exifAttributes() {
+        YailList attributes = new YailList();
+        for (String field : EXIF_ATTRIBUTES)
             attributes.add(field);
         return attributes;
     }
-//
-//    @SimpleFunction
-//    public String getDate(ExifInterface image) {
-//        return image.getAttribute(ExifInterface.TAG_DATETIME);
-//    }
+
+    @SimpleEvent(description = "Handle errors that occur from the library")
+    public void ErrorOccurred(String error) {
+        EventDispatcher.dispatchEvent(this, "ErrorOccured", error);
+    }
 }
